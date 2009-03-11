50de4651855de77b97dc8a3c3cb2aa5bb8862ceb
Kai-Ming Chang <kaiming@gmail.com>
2009-03-11 03:27:49 +0000

    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/RefPlus@37891 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/rmaref.predict.R
+++ R/rmaref.predict.R
@@ -7,6 +7,7 @@ PM<-sweep(PM,1,unlist(p.e))
 pm(Future)<-PM
 PMlist<-lapply(PMindex,function(x,y) intensity(y)[x,],Future)
 future<-t(sapply(PMlist,colMedians))
+if(nrow(future)==1) {future <- t(future)}
 colnames(future)<-sampleNames(Future)
 return(future)
 }
